f8a825166f875a569e4c7e899477b4e40125e3c9
PatelAvadh <patelavadh1231@gmail.com>
2022-06-29 15:33:51 -0400

    Added Backend Match API

--- src/main/java/io/project/Repository/MatchRepository.java
+++ src/main/java/io/project/Repository/MatchRepository.java
@@ -1,5 +1,6 @@
 package io.project.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
@@ -12,8 +13,12 @@ public interface MatchRepository extends CrudRepository<Match, Long> {
 
 	List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
 	
+	List<Match> getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(String team1, LocalDate date1, LocalDate date2, String team2, LocalDate date3, LocalDate date4);
+	
 	default List<Match> findLatestMatchesByTeam(String teamName, int count){
 		return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, 4));
 	}
+	
+	
 
 }
--- src/main/java/io/project/controller/TeamController.java
+++ src/main/java/io/project/controller/TeamController.java
@@ -1,13 +1,16 @@
 package io.project.controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateConverter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.project.Repository.MatchRepository;
@@ -31,5 +34,12 @@ public class TeamController {
 		System.out.println(team);
 		team.setMatches(matchRepository.findLatestMatchesByTeam(teamName, 4));
 		return team;
+	}
+	
+	@GetMapping("/team/{teamName}/matches")
+	public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+		LocalDate startDate = LocalDate.of(year, 1, 1);
+		LocalDate endDate = LocalDate.of(year+1, 1, 1);
+		return matchRepository.getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(teamName, startDate, endDate, teamName, startDate, endDate);
 	}
 }
